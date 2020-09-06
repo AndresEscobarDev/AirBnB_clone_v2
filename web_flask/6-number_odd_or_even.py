b00eebee53140c7a2c7ee71189b4fb1ce5f7fe0c
Andres Felipe Escobar Cardona <felipeescobar15@gmail.com>
2020-09-06 20:00:01 +0000

    Task 0,1,2,3,4,5,6

---
+++ web_flask/6-number_odd_or_even.py
@@ -0,0 +1,48 @@
+#!/usr/bin/python3
+""" Script that starts a Flask web application. """
+
+from flask import Flask
+from flask import render_template
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    """ Returns a string at the root route """
+    return "Hello HBNB!"
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """ Returns a string at the /hbnb route """
+    return "HBNB"
+
+@app.route('/c/<text>', strict_slashes=False)
+def c(text):
+    """ Returns a string at the /c/<text> route """
+    text = text.replace('_', ' ')
+    return "C {}".format(text)
+
+@app.route('/python', defaults={'text': 'is cool'}, strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def python(text):
+    """ Returns a string at the /python/<text> route """
+    text = text.replace('_', ' ')
+    return "Python {}".format(text)
+
+@app.route('/number/<int:n>', strict_slashes=False)
+def number(n):
+    """ Returns a string at the /number/<n> route """
+    return "{} is a number".format(n)
+
+@app.route('/number_template/<int:n>', strict_slashes=False)
+def number_template(n):
+    """ Returns a string at the /number_template/<n> route """
+    return render_template('5-number.html', n=n)
+
+@app.route('/number_odd_or_even/<int:n>', strict_slashes=False)
+def number_odd_or_even(n):
+    """ Returns a string at the /number_odd_or_even/<n> route """
+    return render_template('6-number_odd_or_even.html', n=n)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
